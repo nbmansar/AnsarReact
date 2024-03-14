a15cd046c26b824de9b56e729c221ee61609aed5
Mohamed Ansar N <nb.mdansar@gmail.com>
2024-03-14 06:25:40 +0530

    first upload

---
+++ phase2/src/Components/Home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PlantGrowth from '../PlantGrowth/PlantGrowth'
+
+function Home() {
+  return (
+    <div>home
+    <PlantGrowth />
+    </div>
+  )
+}
+
+export default Home
---
+++ phase2/src/Components/PlantGrowth/PlantGrowth.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../PlantGrowth/growth.css'
+
+function PlantGrowth() {
+  return (
+    <div className="className">
+    <div className="plant">PlantGrowth🤣</div>
+    <div className="btn btn-success" style={{color:'yellow'}}>Yellow</div>
+    </div>
+  )
+}
+
+export default withAuthrize(PlantGrowth)
